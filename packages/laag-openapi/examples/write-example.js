ae9550f79b813e3a7403ffcf7eef17625296cfae
Brett Schwarz <brett.schwarz@gmail.com>
2020-11-24 17:08:04 -0800

    docs: adds write example

---
+++ packages/laag-openapi/examples/write-example.js
@@ -0,0 +1,27 @@
+const Openapi = require('../lib/laag-openapi');
+
+//
+// Create empty doc
+//
+const doc = new Openapi();
+
+//
+// Set some general properties
+//
+doc.title = "My Awesome API";
+doc.version = "1.0.0";
+doc.description = "This is just an API to showcase laag";
+const contact = {name: 'Brett', email: 'abc@compay.com'};
+doc.contact = contact;
+let servers = [];
+servers.push({url: 'some.url.com', description: 'This is the main URL to hit the API with'});
+doc.servers = servers;
+const ext = {'x-my-custom-extension': 'My Value'};
+doc.rootExtensions = ext;
+//
+// add a path (Note: this is just a partial path definition)
+//
+const path = {'get': {description: 'This fetches myresource', operationId: 'getMyresource', responses: {'200': {description: 'Success'}}}};
+doc.appendPath('/myresounce', path);
+
+console.log(doc.getDefinition('prettyjson'));
