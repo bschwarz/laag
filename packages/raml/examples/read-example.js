353fef3244758fdbeece8d59f5e0bef9d4fd3903
Brett Schwarz <brett.schwarz@gmail.com>
2020-11-25 22:10:54 -0800

    chore: cleanup

--- packages/raml/examples/read-example.js
+++ packages/raml/examples/read-example.js
@@ -3,7 +3,6 @@ const Raml = require('../lib/laag-raml');
 let data = fs.readFileSync('examples/example.raml', 'utf8');
 doc = new Raml(data);
 
-console.log(Object.keys(doc.getDefinition()));
 console.log(`Displaying API: ${doc.title} v${doc.version}`);
 console.log('Paths defined in this API:');
 for (const pathname of doc.getPathNames()) {
