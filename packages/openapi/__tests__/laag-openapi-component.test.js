4e04c7a778eb0a50bd6dc426541181b3c6593bd3
Brett Schwarz <brett.schwarz@gmail.com>
2020-12-16 21:25:20 -0800

    test: adds test to see if Header members works

--- packages/openapi/__tests__/laag-openapi-component.test.js
+++ packages/openapi/__tests__/laag-openapi-component.test.js
@@ -17,3 +17,17 @@ test('Get/Set header description', () => {
     console.log(head1.getHeader());
     expect(head1.description).toBe('This is my description');
 });
+//
+// Test get all description
+//
+test('Get/Set header description', () => {
+    let head1 = new Header('MyCustomHeader1');
+    let head2 = new Header('MyCustomHeader2');
+    head1.description = "description1"
+    head2.description = "description2"
+    let ret = [];
+    for (let x of Header.members) {
+        ret.push(x.description);
+    }
+    expect(ret.length).toBe(3);
+});
